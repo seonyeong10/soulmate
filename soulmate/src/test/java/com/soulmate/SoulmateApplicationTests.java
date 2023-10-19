7e52473a2dedb6187e737345a0fec3959cdae50f
seonyeong10 <suny951013@naver.com>
2023-10-19 15:53:03 +0900

    Initial commit

---
+++ soulmate/src/test/java/com/soulmate/SoulmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
